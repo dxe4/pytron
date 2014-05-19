cc7b1e223f4370cdec4b027ee538b7bf70dd1ddc
charalambosp <papaloizouc@gmail.com>
2014-05-19 22:32:15 +0100

    printing players

--- tron.py
+++ tron.py
@@ -5,29 +5,36 @@ import math
 
 SIZE = 100
 
-board = [i for i in range(0, SIZE) for j in range(0, SIZE)]
+colors = [curses.COLOR_BLUE, curses.COLOR_GREEN, curses.COLOR_RED,
+          curses.COLOR_YELLOW, curses.COLOR_MAGENTA]
+
 
 class Board(object):
     def __init__(self, players_size=4):
-        if players_size not in (2, 4, 6, 8):
-            raise ValueError("Players must be one of the following: 2, 4, 6, 8")
+        if players_size not in (2, 4, 6):
+            raise ValueError("Players must be one of the following: 2, 4, 6")
 
-        self.players = []
+        self.board = [i for i in range(0, SIZE) for j in range(0, SIZE)]
+        players = []
         for i in range(int(players_size / 2)):
-            self.players.extend(self.player_set(self.players))
+            players.extend(self.player_set(players))
 
+        self.players = [{"color": count, "points": [i]}
+                        for count, i in enumerate(players)]
+        print(self.players)
 
     def random_player(self):
         return randint(0, SIZE - 1), randint(0, SIZE - 1)
 
-    def player_exists(self, x ,y, skip):
+
+    def player_exists(self, x, y, skip):
         if not skip:
             return False
 
-        abs_points = [math.fabs(x), math.fabs(y)]
+        all_points = [x, y, SIZE - x, SIZE - y]
 
         try:
-            next((j for i in skip for j in i if math.fabs(j) in abs_points))
+            next((j for i in skip for j in i if math.fabs(j) in all_points))
             return True
         except StopIteration:
             return False
@@ -36,7 +43,7 @@ class Board(object):
         xa, ya = self.random_player()
         while self.player_exists(xa, ya, skip):
             xa, ya = self.random_player()
-        xb, yb = -xa, -ya
+        xb, yb = SIZE - xa, SIZE - ya
         return (xa, ya), (xb, yb)
 
 
@@ -49,6 +56,11 @@ class Game(object):
         stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
+
+        for count, i in enumerate(colors):
+            curses.start_color()
+            curses.init_pair(count+1, i, curses.COLOR_BLACK)
+
         try:
             self.loop(stdscr)
         except Exception as e:
@@ -58,13 +70,15 @@ class Game(object):
 
     def loop(self, stdscr):
 
-        for x in range(1, SIZE):
-            for y in range(1, SIZE):
-                stdscr.addstr(int(x / 2), y, "-")
+        for player in self.board.players:
+            color = player["color"]
+            for point in player["points"]:
+                stdscr.addstr(int(point[0] / 2), point[1], "*", curses.color_pair(color+1))
+
         stdscr.keypad(True)
 
         stdscr.refresh()
-        time.sleep(5)
+        time.sleep(4)
 
     def stop(self, stdscr):
         curses.nocbreak()
