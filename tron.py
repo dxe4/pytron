ff6b9232baf60d8c36b179fe7ca7c1a9b9167265
charalambosp <papaloizouc@gmail.com>
2014-05-29 22:12:33 +0100

    fixed random player

--- tron.py
+++ tron.py
@@ -16,14 +16,36 @@ possible_directions = {i: vertical for i in horizontal}
 possible_directions.update({i: horizontal for i in vertical})
 
 
-
 class Player(object):
-
-
     def __init__(self, color, points, direction=None):
         self.color = color
         self.points = points
         self.direction = direction or directions[randint(0, 3)]
+
+
+class FairRandom(object):
+    @classmethod
+    def make_random_players(cls, player_size):
+        players = []
+        for i in range(int(player_size / 2)):
+            new_players = cls.player_set(players)
+            # If any of players already exist make new ones
+            while [i for i in new_players if i in players]:
+                new_players = cls.player_set(players)
+
+            players.extend(new_players)
+
+        return [Player(count, [points]) for count, points in enumerate(players)]
+
+    @classmethod
+    def random_player(cls):
+        return randint(0, SIZE - 1), randint(0, SIZE - 1)
+
+    @classmethod
+    def player_set(cls, skip=None):
+        xa, ya = cls.random_player()
+        xb, yb = SIZE - xa, SIZE - ya
+        return (xa, ya), (xb, yb)
 
 
 class Board(object):
@@ -32,39 +54,7 @@ class Board(object):
             raise ValueError("Players must be one of the following: 2, 4, 6")
 
         self.board = [i for i in range(0, SIZE) for j in range(0, SIZE)]
-        self.players = players or self.make_random_players(player_size)
-
-    def make_random_players(self, player_size):
-        players = []
-        for i in range(int(player_size / 2)):
-            players.extend(self.player_set(players))
-
-        return [Player(count, [points]) for count, points in enumerate(players)]
-
-    def random_player(self):
-        return randint(0, SIZE - 1), randint(0, SIZE - 1)
-
-
-    def player_exists(self, x, y, skip):
-        if not skip:
-            return False
-
-        all_points = [x, y, SIZE - x, SIZE - y]
-
-        try:
-            next((j for i in skip for j in i if math.fabs(j) in all_points))
-            return True
-        except StopIteration:
-            return False
-
-    def player_set(self, skip=None):
-        xa, ya = self.random_player()
-
-        while self.player_exists(xa, ya, skip):
-            xa, ya = self.random_player()
-        xb, yb = SIZE - xa, SIZE - ya
-
-        return (xa, ya), (xb, yb)
+        self.players = players or FairRandom.make_random_players(player_size)
 
 
 class Game(object):
@@ -92,9 +82,9 @@ class Game(object):
     def loop(self, stdscr):
 
         for player in self.board.players:
-            color = player.color
+            color = curses.color_pair(player.color)
             for point in player.points:
-                stdscr.addstr(int(point[0] / 2), point[1], "*", curses.color_pair(color))
+                stdscr.addstr(int(point[0] / 2), point[1], "*", color)
 
         stdscr.keypad(True)
 
